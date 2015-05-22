54a4918853c01c8b1b1bc58a265a8858fe8395bd
吕大卫 <lvdawei@lvdaweideMacBook-Pro.local>
2015-05-22 22:59:00 +0800

    demo完成

--- ChineseZodiac/ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ChineseZodiac/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mylabel: UITextField!
+    
+    @IBOutlet weak var myimg: UIImageView!
+    
+    let offset = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        mylabel.resignFirstResponder()
+    }
 
+    @IBAction func myclick(sender: AnyObject) {
+       mylabel.resignFirstResponder()
+        if let year = mylabel.text.toInt(){
+            var imgNum = (year - offset) % 12
+            myimg.image = UIImage(named: String(imgNum))
+        }
+    }
 
 }
 
